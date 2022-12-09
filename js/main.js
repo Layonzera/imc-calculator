ce794f89a6f86846829d07f06bb28d6fe77aa179
Adryann Layon <adryann.layon7@gmail.com>
2022-12-09 16:50:40 -0300

    feature: close alert error

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-// import
 import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+import { notANumber, calculateIMC } from './utils.js'
 
-// variáveis -variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -12,13 +15,22 @@ form.onsubmit = event => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  const result = IMC(weight, height)
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+  if (weightOrHeightIsNotANumber) {
+    AlertError.open()
+    return
+  }
+
+  AlertError.close()
+
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
   const message = `Seu IMC é de ${result}`
 
   Modal.message.innerText = message
   Modal.open()
-}
-
-function IMC(weight, height) {
-  return `${(weight / (height / 100) ** 2).toFixed(2)}`
 }
